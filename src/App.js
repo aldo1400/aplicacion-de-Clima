12d17ea9d204835a870087a8ddfdb5920e717559
Aldo Choque <achoque1400@gmail.com>
2018-08-07 20:21:41 -0500

    Componente error y clima

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario'
+import Error from './componentes/Error';
+import Clima from './componentes/Clima';
 
 class App extends Component {
 
   state={
-    error:''
+    error:'',
+    consulta:{},
+    resultado:{}
   }
 
+  componentDidUpdate (){
+    this.consultarApi();
+  }
+  
   componentDidMount(){
     this.setState({
       error:false
     })
+  }
+
+  consultarApi=()=>{
+    const {ciudad,pais}=this.state.consulta;
+    if(!ciudad||!pais) return null;
+    // leer la url y agregar el API key
+    const appId='2f02054a4c294f0f2088d6ea49a4212a';
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+    console.log(url);
+    // query con fetch api
+    fetch(url)
+      .then(respuesta=>{
+        return respuesta.JSON();
+      })
+      .then(datos=>{
+        this.setState({
+          resultado:datos
+        })
+      })
+      .catch(error=>{
+        console.log(error);
+      })
+      
   }
 
   datosConsulta=respuesta=>{
@@ -21,11 +52,27 @@ class App extends Component {
       })
     }
     else{
-      console.log('todo correcto');
+      this.setState({
+        consulta:respuesta,
+        error:false
+      })
     }
   }
 
   render() {
+
+    const error=this.state.error;
+    
+    let resultado;
+    if(error){
+      resultado=<Error mensaje="Ambos campos son obligatorios"/>
+    }
+    else{
+      resultado=<Clima resultado={this.state.resultado}/>
+    }
+
+
+
     return (
       <div className="App">
         <Header
@@ -34,6 +81,7 @@ class App extends Component {
         <Formulario
           datosConsulta={this.datosConsulta}
         />
+        {resultado}
       </div>
     );
   }
---
+++ src/componentes/Clima.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Clima extends Component {
+  render() {
+      
+    return (
+      <div className="container">
+        <h1>Desde clima.js</h1>
+      </div>
+    )
+  }
+}
+
+export default Clima;
